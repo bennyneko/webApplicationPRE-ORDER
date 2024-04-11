caba7871461cd41402e4869d6a285f16f3490f73
Landmark-GitHub <l.landmark.mail@gmail.com>
2024-04-11 15:25:36 +0700

    Forget

--- BACKEND/general/models.py
+++ BACKEND/general/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     sku = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    # type = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
