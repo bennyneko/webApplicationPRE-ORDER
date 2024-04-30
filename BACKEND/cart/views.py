2a426531a3dea8e315e25919320a059141a9bff9
bennyneko <bennyneko>
2024-04-30 08:58:28 +0700

    fetch

--- BACKEND/cart/views.py
+++ BACKEND/cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import CartItem
+from products.models import Product  # Import Product model
 
 @login_required
 def view_cart(request):
@@ -11,8 +12,10 @@ def view_cart(request):
 def add_to_cart(request, product_id):
     if request.method == 'POST':
         product = Product.objects.get(pk=product_id)
-        cart_item = CartItem(user=request.user, product=product)
-        cart_item.save()
+        cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
         return redirect('view_cart')
 
 @login_required
